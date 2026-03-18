80e83df1006b242d82b20a42481887162f187d05
2014000029-FrancoPeruch <f.peruch.5ddb@gmail.com>
2026-03-17 19:33:46 -0600

    Add files via upload
    Trabajo1-C#-Programación
    Se realizó el uso de condiciones con IF

---
+++ NumPosNumNeg.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Números Positivos y Negativos");
+        int num;
+
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine("El número es POSITIVO.");
+        else
+            Console.WriteLine("El número es NEGATIVO.");
+    }
+}
